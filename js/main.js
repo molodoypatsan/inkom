6d7321066901d1d4b626dd921b2ed9dbd3729920
molodoypatsan <ledovskihdima2003@gmail.com>
2022-02-02 03:45:14 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ menuBurger.addEventListener('click', () => {
     menuBurger.classList.toggle('active');
     dropdownMenu.classList.toggle('active');
 });
+
+const tab = document.querySelectorAll('[data-tabs-tab]')
+const tabBodys = document.querySelectorAll('[data-tabs-body]')
+
+for (let btn of tab) {
+    btn.addEventListener('click', () => {
+        tab.forEach(item => { item.classList.remove('active') })
+        btn.classList.add('active')
+        tabBodys.forEach(content => {
+            if (content.dataset.tabsBody === btn.dataset.tabsTab) {
+                content.classList.remove('hidden')
+            } else {
+                content.classList.add('hidden')
+            }
+        })
+    })
+}
---
+++ js/sliders.js
@@ -0,0 +1,27 @@
+new Swiper('.catalog__slider-apartments', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
+
+new Swiper('.catalog__slider-newbuilding', {
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+new Swiper('.catalog__slider-rent', {
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+new Swiper('.catalog__slider-country', {
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
